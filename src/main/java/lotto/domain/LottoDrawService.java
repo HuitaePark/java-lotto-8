65e1b1d68355c480cb31145f0b2747857ca0ce55
HuitaePark <qkrgmlxo3174@gmail.com>
2025-10-30 21:22:33 +0900

    feat(LottoDrawService): 입력받은 구매 횟수만큼 로또 번호를 출력하는 기능 구현

---
+++ src/main/java/lotto/domain/LottoDrawService.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class LottoDrawService {
+    private final LottoNumberGenerator lottoNumberGenerator;
+
+    public LottoDrawService(LottoNumberGenerator lottoNumberGenerator) {
+        this.lottoNumberGenerator = lottoNumberGenerator;
+    }
+
+    public List<Lotto> draw(int quantity) {
+        return IntStream.range(0, quantity)
+                .mapToObj(i -> lottoNumberGenerator.generate())
+                .map(Lotto::new)
+                .toList();
+    }
+
+}
---
+++ src/test/java/lotto/domain/LottoDrawServiceTest.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoDrawServiceTest {
+
+    @DisplayName("구매횟수만큼 로또를 발행한다.")
+    @Test
+    void draw_lotto_success() {
+        LottoDrawService lottoDrawService = new LottoDrawService(() -> List.of(1, 2, 3, 4, 5, 6));
+
+        List<Lotto> lottos = lottoDrawService.draw(3);
+
+        assertThat(lottos).hasSize(3);
+    }
+}
