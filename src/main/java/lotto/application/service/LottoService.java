6550e5137092e885e0e0d1fca7b728865589197d
HuitaePark <qkrgmlxo3174@gmail.com>
2025-11-03 00:52:26 +0900

    feat: 로또 구입금액이 숫자인지 확인하는 기능 구현

--- src/main/java/lotto/application/service/LottoService.java
+++ src/main/java/lotto/application/service/LottoService.java
@@ -1,7 +1,10 @@
 package lotto.application.service;
 
+import lotto.application.util.InputParser;
+import lotto.domain.DrawService;
 import lotto.domain.repository.LottoRepository;
 import lotto.domain.value.PurchaseAmount;
+import lotto.infra.RandomLottoNumberGenerator;
 
 public class LottoService {
     private final LottoRepository lottoRepository;
@@ -13,7 +16,19 @@ public class LottoService {
     public int getPurchaseCount(String input) {
         int money = InputParser.parse(input);
         PurchaseAmount purchaseAmount = new PurchaseAmount(money);
-        
-        return purchaseAmount.getQuantity();
+
+        int count = purchaseAmount.getQuantity();
+        issuedLotto(count);
+
+        return count;
+    }
+
+    public String getIssuedTicket() {
+
+    }
+
+    private void issuedLotto(int count) {
+        DrawService drawService = new DrawService(new RandomLottoNumberGenerator());
+        lottoRepository.saveAll(drawService.draw(count));
     }
 }
---
+++ src/main/java/lotto/application/util/InputParser.java
@@ -0,0 +1,16 @@
+package lotto.application.util;
+
+public class InputParser {
+
+
+    public static int parse(String input) {
+        validateNumeric(input);
+        return Integer.parseInt(input);
+    }
+
+    private static void validateNumeric(String input) {
+        if (input.matches("\\d+")) {
+            throw new IllegalArgumentException("구입금액은 숫자로 입력해야 합니다");
+        }
+    }
+}
