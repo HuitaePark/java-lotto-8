84ff2ee70ddb0db76d4d343ef4dc9626ce04c9be
HuitaePark <qkrgmlxo3174@gmail.com>
2025-11-03 12:25:48 +0900

    refactor: 에러 메세지가 "[ERROR]"가 나오도록 수정

--- src/main/java/lotto/application/util/InputParser.java
+++ src/main/java/lotto/application/util/InputParser.java
@@ -20,13 +20,13 @@ public class InputParser {
 
     private static void validateNumeric(String input) {
         if (!input.matches("\\d+")) {
-            throw new IllegalArgumentException("구입금액은 숫자로 입력해야 합니다");
+            throw new IllegalArgumentException("[ERROR] 구입금액은 숫자로 입력해야 합니다");
         }
     }
 
     private static void validateComma(String input) {
         if (!input.matches("^\\d+(,\\d+){5}$")) {
-            throw new IllegalArgumentException("숫자 6개를 쉼표로 구분해 입력해야 합니다. 예: 1,2,3,4,5,6");
+            throw new IllegalArgumentException("[ERROR] 숫자 6개를 쉼표로 구분해 입력해야 합니다. 예: 1,2,3,4,5,6");
         }
     }
 }
--- src/main/java/lotto/domain/value/BonusNumber.java
+++ src/main/java/lotto/domain/value/BonusNumber.java
@@ -8,7 +8,7 @@ public record BonusNumber(int value) {
 
     private void verifyOutOfLange(int number) {
         if (number > 45 || number < 1) {
-            throw new IllegalArgumentException("보너스 넘버는 1에서 45 사이여야 합니다.");
+            throw new IllegalArgumentException("[ERROR] 보너스 넘버는 1에서 45 사이여야 합니다.");
         }
     }
 
