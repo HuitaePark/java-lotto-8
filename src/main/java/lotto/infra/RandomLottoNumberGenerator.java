ce0f27a11c5e83560c9dcfdf9b5a21800faebf32
HuitaePark <qkrgmlxo3174@gmail.com>
2025-11-03 16:20:40 +0900

    refactor(RandomLottoNumberGenerator): 매직넘버 상수로 리팩터링

--- src/main/java/lotto/infra/RandomLottoNumberGenerator.java
+++ src/main/java/lotto/infra/RandomLottoNumberGenerator.java
@@ -5,8 +5,12 @@ import java.util.List;
 import lotto.domain.LottoNumberGenerator;
 
 public class RandomLottoNumberGenerator implements LottoNumberGenerator {
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_COUNT = 6;
+
     @Override
     public List<Integer> generate() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_COUNT);
     }
 }
